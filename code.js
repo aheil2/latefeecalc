4b1d7b7521cfdadae5e96bb153a5e75baba1f73f
aheil2 <71800212+aheil2@users.noreply.github.com>
2020-10-28 21:16:43 -0500

    jQuery update

--- code.js
+++ code.js
@@ -1,23 +1,28 @@
-function lateFeeCalc()
-{
-    // Get days late
-    var daysLate = prompt('How many days late is your return?');
-    // Parse to number
-    daysLate = parseFloat(daysLate);
-    // Get Amount of Books
-    var books = prompt('How many books do you have to return?');
-    // Parse to number
-    books = parseFloat(books);
-    // Get Amount of DVDs
-    var dvds = prompt('How many DVDs do you have to return?');
-    // Parse to number
-    dvds = parseFloat(dvds);
-    // calculate book fees
-    var bookfee = (books * 0.25) * daysLate;
-    // calculate dvd fees
-    var dvdfee = (dvds * 0.50) * daysLate;
-    // calculate total fees
-    var totalfee = bookfee + dvdfee;
-    // display total fees
-    alert(`You currently owe $${totalfee.toFixed(2)} in late fees.`);
-}
+$(document).ready(function () {
+    // event handlers
+    $("button").click(lateFeeCalc);
+
+    function lateFeeCalc() {
+        // Get days late
+        var daysLate = $("#daysLate").val();
+        // Parse to number
+        daysLate = parseFloat(daysLate);
+        // Get Amount of Books
+        var books = $("#books").val();
+        // Parse to number
+        books = parseFloat(books);
+        // Get Amount of DVDs
+        var dvds = $("#dvds").val();
+        // Parse to number
+        dvds = parseFloat(dvds);
+        // calculate book fees
+        var bookfee = (books * 0.25) * daysLate;
+        // calculate dvd fees
+        var dvdfee = (dvds * 0.50) * daysLate;
+        // calculate total fees
+        var totalfee = bookfee + dvdfee;
+        // display total fees
+        $("#totalfee").text(totalfee.toFixed(2));
+        $(".output").show();
+    }
+});
